40712187091b03c7e7697521a8928a2f7e76dfe0
kranz fontanilla <me@kranznikolai.com>
2018-10-01 09:30:48 -0400

    repeater function  not done

--- example/js/app.js
+++ example/js/app.js
@@ -1,6 +1,10 @@
 infinite.controller('main',function(){
     console.log('main controller');
 });
-infinite.controller('sub',function(){
+infinite.controller('sub',function(scope){
+    scope.name='test';
+    scope.y=[];
+    console.log(scope);
+
     console.log('sub controller');
 });
--- example/js/infinitejs.js
+++ example/js/infinitejs.js
@@ -1,21 +1,16 @@
-
-
+'use strict'
 var rootScope ={}; 
 var infinite = (function(){
     return{
         controller:controller
     };
-
-    
     function init(controller,vmmodels){
         rootScope[controller] = {};
         vmmodels.forEach(vmmodel => {
             var propToBind = vmmodel.getAttribute('data-i-bind');
             addScopeProp(propToBind);
-    
             vmmodel.addEventListener('change',function(){
                 rootScope[controller][propToBind] = vmmodel.value;
-                console.log('test1');
             });
             vmmodel.addEventListener('keyup',function(){
                 rootScope[controller][propToBind] = vmmodel.value;
@@ -33,11 +28,11 @@ var infinite = (function(){
                     {
                     set: function(newValue){
                         value = newValue;
-                        console.log('test');
                         vmmodels.forEach(function(element){
                             if(element.getAttribute('data-i-bind')=== prop){
                                 element.value= newValue;
                                 element.innerHTML = newValue;
+                   
                             }
                         });
                     },
@@ -50,22 +45,37 @@ var infinite = (function(){
         }
    
     }
-    
-    
-    //not yet implemented
     function controller(controllername, controllerfunction){
         document.addEventListener('DOMContentLoaded',function(){
             var controllerElement = document.querySelectorAll('[data-i-controller="'+controllername+'"]');
-            console.log(controllerElement);
             controllerElement.forEach(function(element){
-                
                var vmmodels= element.querySelectorAll('[data-i-bind]');
-
                init(controllername,vmmodels);
-               console.log(element);
+               repeaterhandler(element,controllername);
             });
-           controllerfunction();
+           controllerfunction(rootScope[controllername]);
+
         });
+    }
+    function repeaterhandler(controllerElement, controllername){
+        var repeaters = controllerElement.querySelectorAll('[data-i-repeat]');
+        if(typeof(rootScope)!=='undefined'){
+          if(typeof(rootScope[controllername]!=='undefined')){
+            var scope = rootScope[controllername];
+         
+            repeaters.forEach(repeater => {
+              var repeaterattrb = repeater.getAttribute('data-i-repeat');
+              console.log(repeaterattrb)
+              if(/^[a-z-A-Z]* in [a-z-A-Z]*$/.test(repeaterattrb)){
+                var metadata = repeaterattrb.split(' ');
+                console.log(scope['y']);
+                if(Array.isArray()){
+                    console.log('test');
+                }
+              }  
+            });
+          }
+        }
 
     }
 })();
